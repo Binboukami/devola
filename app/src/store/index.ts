8e0ae38ab73eb29b7213820a040cd2578b662456
Luiz Alex Butkeivicz <luisalexsh@hotmail.com>
2022-02-28 20:48:32 -0300

    add initial vuex state

--- app/src/store/index.ts
+++ app/src/store/index.ts
@@ -4,9 +4,52 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+  state: {
+    todos: Array(0),
+  },
+  getters: {
+    $getTodo: (state) => {
+      return state.todos.filter((todo) => {
+        return todo.status == false;
+      });
+    },
+  },
+  mutations: {
+    deleteTodo(state, payload) {
+      state.todos.splice(state.todos.indexOf(payload), 1);
+    },
+    getLocalStorage(state) {
+      if (localStorage.length > 0 && localStorage.todos) {
+        state.todos = JSON.parse(localStorage.todos);
+      }
+      // Tratativa de erro
+    },
+    setLocalStorage(state) {
+      if (state.todos) {
+        localStorage.setItem("todos", JSON.stringify(state.todos));
+      }
+    },
+    updateLocalStorage(state, payload) {
+      if (state.todos) {
+        state.todos.push({ ...payload, id: Date.now() });
+      }
+    },
+  },
+  actions: {
+    async deleteTodo({ commit, dispatch }, payload) {
+      await commit("deleteTodo", payload);
+      await dispatch("setLocalStorage");
+    },
+    async getLocalStorage({ commit }) {
+      await commit("getLocalStorage");
+    },
+    async setLocalStorage({ commit }) {
+      await commit("setLocalStorage");
+    },
+    async updateLocalStorage({ commit, dispatch }, payload) {
+      await commit("updateLocalStorage", payload);
+      await dispatch("setLocalStorage");
+    },
+  },
   modules: {},
 });
