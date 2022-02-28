7d9dae2aa2e5f3676251d4e6a0c2b1ffcae4f6e1
Luiz Alex Butkeivicz <luisalexsh@hotmail.com>
2022-02-28 20:50:12 -0300

    add initial crud files

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,29 +1,150 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <vs-dialog :loading="this.animState.loading" v-model="addForm">
+      <template #header>
+        <h4>Add a new todo item</h4>
+      </template>
+      <form class="con-form" @submit="addTodo" ref="todoForm">
+        <vs-input
+          class="todo-form__title"
+          v-model="newTodo.title"
+          label-placeholder="Title"
+          required
+        >
+        </vs-input>
+
+        <vs-input
+          type="text"
+          ref="todoInput"
+          v-model="newTodo.content"
+          required
+          label-placeholder="What you need to do?"
+          class="todo-form__content"
+        >
+        </vs-input>
+
+        <vs-input
+          type="date"
+          v-model="newTodo.date"
+          label-placeholder="When it is due? (optional)"
+          class="todo-form__content"
+        >
+        </vs-input>
+        <div class="footer-dialog">
+          <vs-button block type="submit" value="submit"> Add </vs-button>
+          <vs-button block @click.prevent="resetForm"> Cancel </vs-button>
+        </div>
+      </form>
+
+      <template #footer> </template>
+    </vs-dialog>
+
+    <vs-row class="center grid">
+      <vs-col vs-type="flex" w="12">
+        <h1>VueJS CRUD Application</h1>
+      </vs-col>
+
+      <vs-tooltip>
+        <vs-button @click="addForm = !addForm" icon>
+          <i class="bx bx-plus-medical"></i>
+        </vs-button>
+        <template #tooltip> Add new </template>
+      </vs-tooltip>
+    </vs-row>
+
+    <vs-row class="todos" justify="center">
+      <vs-col w="12">
+        <h1>Todos</h1>
+      </vs-col>
+      <vs-col w="8" v-for="todo in this.$getTodo" :key="todo.id">
+        <todoItem
+          :todo="todo"
+          :editMode="editMode"
+          @edit="editMode = !editMode"
+          @update="updateTodo($event, todo)"
+          @delete="deleteTodo(todo)"
+        ></todoItem>
+      </vs-col>
+    </vs-row>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { mapGetters, mapMutations, mapActions } from "vuex";
+import todoItem from "./components/todoItem.vue";
 
 export default Vue.extend({
   name: "App",
-  components: {
-    HelloWorld,
+  components: { todoItem },
+  mounted() {
+    this.getLocalStorage();
+  },
+  methods: {
+    async updateTodo(input: any, todo: any) {
+      let propName = input.name;
+      todo[propName] = input.value;
+      console.log(todo[propName]);
+      await this.setLocalStorage();
+    },
+    async addTodo(e: any) {
+      if (!this.newTodo) {
+        return;
+      }
+      await this.updateLocalStorage(this.newTodo);
+      // this.validateForm(e);
+      this.resetForm();
+    },
+    validateForm(e: any) {
+      if (this.newTodo.title && this.newTodo.content) {
+        return;
+      } else {
+        console.log("erro");
+      }
+      e.preventDefault;
+    },
+    resetForm(): void {
+      this.newTodo = {
+        id: Date.now,
+        title: "",
+        content: "",
+        date: null,
+        status: false,
+      };
+      this.addForm = !this.addForm;
+    },
+    ...mapMutations([]),
+    ...mapActions([
+      "getLocalStorage",
+      "setLocalStorage",
+      "updateLocalStorage",
+      "deleteTodo",
+    ]),
+  },
+  data() {
+    return {
+      animState: {
+        loading: false,
+      },
+      addForm: false,
+      newTodo: {
+        id: Date.now,
+        title: "",
+        content: "",
+        date: null,
+        status: false,
+      },
+      editMode: false,
+    };
+  },
+  computed: {
+    ...mapGetters(["$getTodo"]),
   },
 });
 </script>
 
 <style lang="scss">
+@import "./scss/index.scss";
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ app/src/components/todoItem.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="todoItem">
+    <vs-card type="1" :class="['todo-card']">
+      <template #text>
+        <div class="todo-card__checkbox">
+          <vs-checkbox
+            class="todo-card__status"
+            @click="$emit('delete')"
+            :value="todo.status"
+            name="status"
+          ></vs-checkbox>
+        </div>
+        <div class="todo-card__content">
+          <vs-input
+            type="date"
+            class="todo-card__edit-field edit-field__date"
+            v-if="editMode"
+            :value="todo.date"
+            name="date"
+            @change="$emit('update', $event.target)"
+          >
+          </vs-input>
+
+          <h5 v-if="!editMode">{{ todo.date }}</h5>
+          <vs-input
+            class="todo-card__edit-field edit-field__title"
+            v-if="editMode"
+            :value="todo.title"
+            name="title"
+            @change="$emit('update', $event.target)"
+          >
+          </vs-input>
+          <h3 v-if="!editMode">
+            {{ todo.title }}
+          </h3>
+          <vs-input
+            class="todo-card__edit-field edit-field__content"
+            v-if="editMode"
+            :value="todo.content"
+            name="content"
+            @change="$emit('update', $event.target)"
+          >
+          </vs-input>
+          <p v-if="!editMode">{{ todo.content }}</p>
+        </div>
+        <div class="todo-card__interactions">
+          <vs-tooltip>
+            <vs-button icon>
+              <i
+                :class="['bx', editMode ? 'bx-check' : 'bxs-pencil']"
+                @click="$emit('edit')"
+              ></i>
+            </vs-button>
+            <template #tooltip>
+              {{ editMode ? "Confirm" : "Edit" }}
+            </template>
+          </vs-tooltip>
+        </div>
+      </template>
+    </vs-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: {
+    todo: {
+      type: Object,
+    },
+    editMode: Boolean,
+  },
+};
+</script>
+
+<style lang="scss"></style>
