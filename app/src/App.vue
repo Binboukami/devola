b7d799c47e984634c65d669ec08313604e8e5e2d
Luiz Alex Butkeivicz <luisalexsh@hotmail.com>
2022-03-02 22:12:05 -0300

    update html structure

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,38 +1,34 @@
 <template>
   <div id="app">
-    <div class="center examplex">
-      <vs-navbar
-        not-line
-        color="#7d33ff"
-        text-white
-        square
-        center-collapsed
-        v-model="active"
-      >
-        <vs-navbar-item :active="active == 'todos'" id="todos">
-          Todos
+    <vs-navbar center-collapsed v-model="active">
+      <template #left>
+        <vs-navbar-item :active="active == 'docs'" id="docs">
+          <a href="https://github.com/Binboukami/devola" target="blank">
+            Documentation
+          </a>
         </vs-navbar-item>
-        <vs-navbar-item>
-          <vs-button class="navbar__button" animation-type="vertical">
-            <i class="bx bxs-quote-alt-right"></i>
-            <template #animate> Devola </template>
-          </vs-button>
-        </vs-navbar-item>
-        <vs-navbar-item :active="active == 'license'" id="license">
-          Github
-        </vs-navbar-item>
-      </vs-navbar>
-      <div class="square">
-        <div class="child">child 1</div>
-        <div class="child">child 2</div>
-      </div>
-    </div>
+      </template>
+      <img src="/logo2.png" alt="DEVOLA" />
+      <template #right>
+        <vs-button icon
+          ><a href="https://github.com/Binboukami" target="blank"></a
+          ><i class="bx bxl-github"></i
+        ></vs-button>
+        <vs-button>
+          <a href="mailto:luiz.butkeivicz@gmail.com" target="blank">Contact</a>
+        </vs-button>
+      </template>
+    </vs-navbar>
 
-    <vs-dialog :loading="this.animState.loading" v-model="addForm">
+    <vs-dialog :loading="this.loading" v-model="addForm">
       <template #header>
         <h4>Add a new todo item</h4>
       </template>
-      <form class="con-form" @submit="addTodo" ref="todoForm">
+      <form
+        class="con-form todo-form-add"
+        @submit.prevent="addTodoItem"
+        ref="todoForm"
+      >
         <vs-input
           class="todo-form__title"
           v-model="newTodo.title"
@@ -40,12 +36,10 @@
           required
         >
         </vs-input>
-
         <vs-input
           type="text"
           ref="todoInput"
           v-model="newTodo.content"
-          required
           label-placeholder="What you need to do?"
           class="todo-form__content"
         >
@@ -55,7 +49,7 @@
           type="date"
           v-model="newTodo.date"
           label-placeholder="When it is due? (optional)"
-          class="todo-form__content"
+          class="todo-form__date"
         >
         </vs-input>
         <div class="footer-dialog">
@@ -63,39 +57,70 @@
           <vs-button block @click.prevent="resetForm"> Cancel </vs-button>
         </div>
       </form>
-
-      <template #footer> </template>
     </vs-dialog>
 
-    <vs-row class="center grid">
-      <vs-col vs-type="flex" w="12">
-        <h1>VueJS CRUD Application</h1>
-      </vs-col>
+    <!-- If there are no todos -->
+    <vs-row
+      justify="center"
+      class="todos todos-empity"
+      v-if="!this.$getTodos.length"
+    >
+      <vs-col w="12">
+        <h2>Ops!</h2>
+        <i class="todos-empity__icon bx bx-bot"></i>
+        <h4>Looks like you don't have any pending todo items.</h4>
+        <p>This might be a good or bad thing.</p>
 
-      <vs-tooltip>
-        <vs-button @click="addForm = !addForm" icon>
-          <i class="bx bx-plus-medical"></i>
-        </vs-button>
-        <template #tooltip> Add new </template>
-      </vs-tooltip>
+        <h5>Try adding something new to do!</h5>
+      </vs-col>
+      <vs-col class="app-interactions" w="12">
+        <vs-button @click="addForm = !addForm" icon> Add new todo </vs-button>
+      </vs-col>
     </vs-row>
 
-    <vs-row class="todos" id="todos" justify="center">
-      <vs-col w="12">
-        <h1>Todos</h1>
+    <!-- If there are todos -->
+    <vs-row
+      class="todos"
+      id="todos"
+      justify="center"
+      v-if="this.$getTodos.length"
+    >
+      <vs-col class="app-interactions" w="10">
+        <vs-button @click="addForm = !addForm" icon>
+          <i class="bx bx-plus-medical"> </i>Add todo
+        </vs-button>
       </vs-col>
       <vs-col
         xs="12"
         sm="10"
         lg="8"
-        v-for="todo in this.$getTodo"
+        v-for="todo in this.$getTodos"
+        :key="todo.id"
+      >
+        <todoItem
+          :todo="todo"
+          @toggleStatus="updateTodoStatus(todo)"
+          @edit="updateTodoItem(todo)"
+          @update="watchInput($event, todo)"
+          @delete="deleteTodo(todo)"
+        ></todoItem>
+      </vs-col>
+    </vs-row>
+
+    <vs-row class="todos" id="todos" justify="center">
+      <vs-col
+        xs="12"
+        sm="10"
+        lg="8"
+        v-for="todo in this.$getDoneTodos"
         :key="todo.id"
       >
         <todoItem
           :todo="todo"
           :editMode="editMode"
-          @edit="editMode = !editMode"
-          @update="updateTodo($event, todo)"
+          @toggleStatus="updateTodoStatus(todo)"
+          @edit="updateTodoItem(todo)"
+          @update="watchInput($event, todo)"
           @delete="deleteTodo(todo)"
         ></todoItem>
       </vs-col>
@@ -111,75 +136,75 @@ import todoItem from "./components/todoItem.vue";
 export default Vue.extend({
   name: "App",
   components: { todoItem },
-  mounted() {
-    this.getLocalStorage();
+  data() {
+    return {
+      active: false,
+      loading: false,
+      addForm: false,
+      newTodo: {
+        title: "",
+        content: "",
+        date: null,
+      },
+      editMode: false,
+    };
+  },
+  async created() {
+    await this.getTodos();
   },
   methods: {
-    async updateTodo(input: any, todo: any) {
-      let propName = input.name;
-      todo[propName] = input.value;
-      console.log(todo[propName]);
-      await this.setLocalStorage();
-    },
-    async addTodo(e: any) {
+    async addTodoItem(e: any) {
       if (!this.newTodo) {
         return;
       }
-      await this.updateLocalStorage(this.newTodo);
-      // this.validateForm(e);
+      this.validateForm(e);
+      this.loading = true;
+      await this.addTodo({ ...this.newTodo, isDone: false, editMode: false });
       this.resetForm();
     },
+    async updateTodoItem(todo: any) {
+      if (todo.is_editing) {
+        await this.updateTodo(todo);
+      }
+      todo.is_editing = !todo.is_editing;
+    },
+    watchInput(input: any, todo: any) {
+      let propName = input.name;
+      todo[propName] = input.value;
+      console.log(todo[propName]);
+    },
     validateForm(e: any) {
-      if (this.newTodo.title && this.newTodo.content) {
+      if (this.newTodo.title) {
         return;
       } else {
-        console.log("erro");
+        console.log(`Error: ${e}`);
       }
       e.preventDefault;
     },
     resetForm(): void {
       this.newTodo = {
-        id: Date.now,
         title: "",
         content: "",
         date: null,
-        status: false,
       };
-      this.addForm = !this.addForm;
+      this.loading = false;
+      this.addForm = false;
     },
     ...mapMutations([]),
     ...mapActions([
-      "getLocalStorage",
-      "setLocalStorage",
-      "updateLocalStorage",
+      "getTodos",
+      "addTodo",
+      "updateTodoStatus",
+      "updateTodo",
       "deleteTodo",
     ]),
   },
-  data() {
-    return {
-      active: false,
-      animState: {
-        loading: false,
-      },
-      addForm: false,
-      newTodo: {
-        id: Date.now,
-        title: "",
-        content: "",
-        date: null,
-        status: false,
-      },
-      editMode: false,
-    };
-  },
   computed: {
-    ...mapGetters(["$getTodo"]),
+    ...mapGetters(["$getTodos", "$getDoneTodos"]),
   },
 });
 </script>
 
 <style lang="scss">
 @import "./scss/index.scss";
-#app {
-}
 </style>
